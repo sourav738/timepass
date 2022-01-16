d23a80044f8dc21b5052deb7cae1a5929735d13d
Sourav <souravsingh738@gmail.com>
2022-01-16 10:32:34 +0530

    latest changes

---
+++ dbconnection.js
@@ -0,0 +1,15 @@
+import mysql from 'mysql'
+require('dotenv').config();
+console.log("USERNAME IS"+process.env.DB_USERNAME);
+var con=mysql.createConnection({
+host:process.env.host,
+user:process.env.DB_USERNAME,
+database:process.env.database
+})
+con.connect((err)=>{
+if(err){
+    throw err;
+}else{
+    console.log("database is connected");
+}
+})
--- index.js
+++ index.js
@@ -1,3 +1,10 @@
 import express from "express";
 const app=express();
-console.log("yersf");
+require('dotenv').config();
+const dbConnect=require('./dbconnection')
+
+const apiRoutes=require('./Routes/index')
+app.use('/api',apiRoutes)
+app.listen(process.env.PORT || 5000,()=>{
+    console.log('sertver is connected');
+})
