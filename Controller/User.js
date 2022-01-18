08b7a9106a444af173c369496c0df009d7f0b33f
Sourav <souravsingh738@gmail.com>
2022-01-18 10:37:13 +0530

    test

--- Controller/User.js
+++ Controller/User.js
@@ -2,4 +2,22 @@ const router=require('express').Router();
 router.get('/',(req,res,next)=>{
 res.send('hello');
 })
+
+router.post('/',(req,res,next)=>{
+    console.log("post requset is coming");
+    res.json({success:'ok'})
+  
+})
+
+router.put('/',(req,res,next)=>{
+    res.json({success:'ok put request is working'})
+})
+
+router.patch('/',(req,res,next)=>{
+    res.json({success:'ok patch request is working'})
+})
+
+router.delete('/',(req,res,next)=>{
+    res.json({success:'ok delete request is working'})
+})
 module.exports=router
