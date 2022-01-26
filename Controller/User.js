c7d272a522ad93cf1292ab937355641e40707eb4
Sourav <souravsingh738@gmail.com>
2022-01-26 19:49:38 +0530

    authentication

--- Controller/User.js
+++ Controller/User.js
@@ -1,4 +1,5 @@
 const router = require('express').Router();
+const bcrypt = require('bcrypt');
 const addUser = require('../validation/validation')
 const con = require('../dbconnection');
 const hashPassword = require('../config/constant');
@@ -20,14 +21,14 @@ router.post('/', addUser.addUser, async (req, res, next) => {
     const email = req.body.email;
     const phone_no = req.body.phone_no;
     const emailExist = `SELECT * FROM users WHERE email='${email}'`
-    console.log({emailExist})
+    console.log({ emailExist })
     con.query(emailExist, function (err, result) {
-        console.log({result});
-        if(result.length>0){
+        console.log({ result });
+        if (result.length > 0) {
             return res.status(200).json({
-                success:'OK',message:'Email is Already Taken.'
+                success: 'OK', message: 'Email is Already Taken.'
             })
-        }else{
+        } else {
             const userInsertQuery = `INSERT INTO users (first_name,middle_name,last_name,email,password,phone_no) VALUES('${first_name}','${middle_name}','${last_name}','${email}','${hashingPassword}',${phone_no})`
             con.query(userInsertQuery, function (err, result) {
                 if (err) throw err;
@@ -36,7 +37,7 @@ router.post('/', addUser.addUser, async (req, res, next) => {
                 })
             });
         }
-       
+
     })
 })
 
@@ -51,9 +52,27 @@ router.patch('/', (req, res, next) => {
 router.delete('/', (req, res, next) => {
     res.json({ success: 'ok delete request is working' })
 })
-router.post('/login',addUser.loginValidation,(req,res,next)=>{
-    res.send({
-        success:'OK'
-    });
+router.post('/login', addUser.loginValidation, (req, res, next) => {
+    const email = req.body.email
+    const userPassword = req.body.password;
+    const fetchUserDetails = `SELECT * FROM users WHERE email='${email}'`;
+    con.query(fetchUserDetails, (err, result) => {
+        if (result.length > 0) {
+            const password = result[0].password
+            try {
+                var passwordCheck = bcrypt.compareSync(userPassword, password);
+
+            } catch (error) {
+                console.log({ error });
+            }
+
+        } else {
+            return res.status(400).json({
+                success: 'OK',
+                msg: 'Incorrect Username Or Password'
+            })
+        }
+    })
+
 })
 module.exports = router
